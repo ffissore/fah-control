8a463363f31000a34f458a709a16d48e5e426a3b
Federico Fissore <federico@fissore.org>
2020-05-27 18:01:03 +0200

    asd

--- setup.py
+++ setup.py
@@ -75,7 +75,8 @@ elif sys.platform == 'win32':
     e = Executable(app, base='Win32GUI', icon='images/FAHControl.ico')
     options = {
         'build_exe': {
-            'build_exe': 'gui'
+            'build_exe': 'gui',
+            'includes': 'gi'
         }
     }
     extra_opts = dict(executables=[e], options=options)
